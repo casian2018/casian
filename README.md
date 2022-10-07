6b444108263dcc82c95806976a7953f826b0cfdc
casian2018 <99408730+casian2018@users.noreply.github.com>
2022-10-07 04:36:56 -0700

    sad

--- README.md
+++
@@ -1,2 +0,0 @@
-# EveryRunning
-Website shoes
--- js/script.js
+++ js/script.js
@@ -1,59 +1,43 @@
-let productPreviewContainer = document.querySelector('.products-preview-container');
-let prodcutPreview = productPreviewContainer.querySelectorAll('.product-preview');
+let menu = document.querySelector('#menu-bar');
+let navbar = document.querySelector('.navbar');
 
-document.querySelectorAll('.products .slide .btn').forEach(detailBtn =>{
-    detailBtn.onclick = () =>{
-        productPreviewContainer.style.display = 'block';
-        let name = detailBtn.getAttribute('data-product');
-        prodcutPreview.forEach(preview =>{
-        let target = preview.getAttribute('data-target');
-        if(name == target){
-        preview.style.display = 'flex';
-        };
-        });
-    };
-    });
+menu.onclick =() =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
 
-    document.querySelectorAll('.products-preview-container .product-preview .bi-x-circle').forEach(close =>{
-    close.onclick = () =>{
-        productPreviewContainer.style.display = 'none';
-        prodcutPreview.forEach(closePreview =>{
-        closePreview.style.display = 'none';
-        });
-    };
-    });
+let slides = document.querySelectorAll('.slide-container');
+let index = 0;
 
-    var swiper = new Swiper(".products-slider", {
-    loop:true,
-    spaceBetween: 20,
-    grabCursor:true,
-    centeredSlides: true,
-    breakpoints: {
-        0: {
-        slidesPerView: 1,
-        },
-        768: {
-        slidesPerView: 2,
-        },
-        991: {
-        slidesPerView: 3,
-        },
-    },
+function next(){
+    slides[index].classList.remove('active');
+    index = (index + 1) % slides.length;
+    slides[index].classList.add('active');
+}
+
+function prev(){
+    slides[index].classList.remove('active');
+    index = (index - 1 + slides.length) % slides.length;
+    slides[index].classList.add('active');
+}
+
+document.querySelectorAll('.featured-image-1').forEach(image_1 =>{
+    image_1.addEventListener('click', () =>{
+        var src = image_1.getAttribute('src');
+        document.querySelector('.big-image-1').src = src;
     });
-    var swiper = new Swiper(".reviews-slider", {
-        loop:true,
-        spaceBetween: 20,
-        grabCursor:true,
-        centeredSlides: true,
-        breakpoints: {
-            0: {
-            slidesPerView: 1,
-            },
-            768: {
-            slidesPerView: 2,
-            },
-            991: {
-            slidesPerView: 3,
-            },
-        },
+});
+
+document.querySelectorAll('.featured-image-2').forEach(image_2 =>{
+    image_2.addEventListener('click', () =>{
+        var src = image_2.getAttribute('src');
+        document.querySelector('.big-image-2').src = src;
     });
+});
+
+document.querySelectorAll('.featured-image-3').forEach(image_3 =>{
+    image_3.addEventListener('click', () =>{
+        var src = image_3.getAttribute('src');
+        document.querySelector('.big-image-3').src = src;
+    });
+});
