3aa6bb931d40b9262be6f6fd36eef5fccfe8be53
casian2018 <99408730+casian2018@users.noreply.github.com>
2022-10-07 04:31:32 -0700

    a

--- js/script.js
+++ js/script.js
@@ -1,72 +1,59 @@
-let body = document.body;
+let productPreviewContainer = document.querySelector('.products-preview-container');
+let prodcutPreview = productPreviewContainer.querySelectorAll('.product-preview');
 
-let profile = document.querySelector('.header .flex .profile');
+document.querySelectorAll('.products .slide .btn').forEach(detailBtn =>{
+    detailBtn.onclick = () =>{
+        productPreviewContainer.style.display = 'block';
+        let name = detailBtn.getAttribute('data-product');
+        prodcutPreview.forEach(preview =>{
+        let target = preview.getAttribute('data-target');
+        if(name == target){
+        preview.style.display = 'flex';
+        };
+        });
+    };
+    });
 
-document.querySelector('#user-btn').onclick = () =>{
-   profile.classList.toggle('active');
-   searchForm.classList.remove('active');
-}
+    document.querySelectorAll('.products-preview-container .product-preview .bi-x-circle').forEach(close =>{
+    close.onclick = () =>{
+        productPreviewContainer.style.display = 'none';
+        prodcutPreview.forEach(closePreview =>{
+        closePreview.style.display = 'none';
+        });
+    };
+    });
 
-let searchForm = document.querySelector('.header .flex .search-form');
-
-document.querySelector('#search-btn').onclick = () =>{
-   searchForm.classList.toggle('active');
-   profile.classList.remove('active');
-}
-
-let sideBar = document.querySelector('.side-bar');
-
-document.querySelector('#menu-btn').onclick = () =>{
-   sideBar.classList.toggle('active');
-   body.classList.toggle('active');
-}
-
-document.querySelector('.side-bar .close-side-bar').onclick = () =>{
-   sideBar.classList.remove('active');
-   body.classList.remove('active');
-}
-
-document.querySelectorAll('input[type="number"]').forEach(InputNumber => {
-   InputNumber.oninput = () =>{
-      if(InputNumber.value.length > InputNumber.maxLength) InputNumber.value = InputNumber.value.slice(0, InputNumber.maxLength);
-   }
-});
-
-window.onscroll = () =>{
-   profile.classList.remove('active');
-   searchForm.classList.remove('active');
-
-   if(window.innerWidth < 1200){
-      sideBar.classList.remove('active');
-      body.classList.remove('active');
-   }
-
-}
-
-let toggleBtn = document.querySelector('#toggle-btn');
-let darkMode = localStorage.getItem('dark-mode');
-
-const enabelDarkMode = () =>{
-   toggleBtn.classList.replace('fa-sun', 'fa-moon');
-   body.classList.add('dark');
-   localStorage.setItem('dark-mode', 'enabled');
-}
-
-const disableDarkMode = () =>{
-   toggleBtn.classList.replace('fa-moon', 'fa-sun');
-   body.classList.remove('dark');
-   localStorage.setItem('dark-mode', 'disabled');
-}
-
-if(darkMode === 'enabled'){
-   enabelDarkMode();
-}
-
-toggleBtn.onclick = (e) =>{
-   let darkMode = localStorage.getItem('dark-mode');
-   if(darkMode === 'disabled'){
-      enabelDarkMode();
-   }else{
-      disableDarkMode();
-   }
-}
+    var swiper = new Swiper(".products-slider", {
+    loop:true,
+    spaceBetween: 20,
+    grabCursor:true,
+    centeredSlides: true,
+    breakpoints: {
+        0: {
+        slidesPerView: 1,
+        },
+        768: {
+        slidesPerView: 2,
+        },
+        991: {
+        slidesPerView: 3,
+        },
+    },
+    });
+    var swiper = new Swiper(".reviews-slider", {
+        loop:true,
+        spaceBetween: 20,
+        grabCursor:true,
+        centeredSlides: true,
+        breakpoints: {
+            0: {
+            slidesPerView: 1,
+            },
+            768: {
+            slidesPerView: 2,
+            },
+            991: {
+            slidesPerView: 3,
+            },
+        },
+    });
