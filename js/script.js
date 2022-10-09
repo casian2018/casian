5b0cb1622eec2744febb21b9b3f89253bbf1633f
casian2018 <99408730+casian2018@users.noreply.github.com>
2022-10-09 02:53:32 -0700

    asa

--- js/script.js
+++ js/script.js
@@ -82,10 +82,3 @@ var swiper = new Swiper(".review-slider", {
       },
     },
 });
-
-<script>
-function clickinner(){
-    var mybtn=document.getElementsByClassName('btn');
-    mybtn.onClick="location.href='about-us.html'";
-};
-</script>
